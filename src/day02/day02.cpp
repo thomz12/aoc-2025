bdd81632d626ecda6c64c739ad2ddf1bbcb89fd5
thomz12 <thomz12@hotmail.nl>
2025-12-02 22:21:59 +0100

    Day 02 finished

--- src/day02/day02.cpp
+++ src/day02/day02.cpp
@@ -8,43 +8,78 @@
 
 namespace Day02 {
     namespace {
-        struct Input {
+        struct Range {
             std::int64_t left;
             std::int64_t right;
         };
 
         auto GetParsedInput(const std::string_view input) {
             return input | std::views::split(',') | std::views::transform(
-                [](auto&& subrange) {
-                    std::cout << std::string(subrange.begin(), subrange.end()) << std::endl;
-                    auto values = subrange | std::views::split('-') |
-                        std::views::transform([](auto &&valRange) {
-                            std::cout << std::string(valRange.begin(), valRange.end()) << std::endl;
-                            return std::stoi(std::string{valRange.begin(), valRange.end()});
-                        }
-                    );
+                       [](auto &&subrange) {
+                           auto values = subrange | std::views::split('-') |
+                                         std::views::transform([](auto &&valRange) {
+                                                 return std::stol(std::string{valRange.begin(), valRange.end()});
+                                             }
+                                         );
 
-                    return Input{
-                        .left = *values.begin(),
-                        .right = *values.begin() + 1
-                    };
-                }
-            );
+                           return Range{
+                               .left = *values.begin(),
+                               .right = *(++values.begin())
+                           };
+                       }
+                   );
         }
     }
 
-    std::int32_t Part1(const std::string_view input) {
+    std::int64_t Part1(const std::string_view input) {
         auto inputs = GetParsedInput(input);
 
-        for (auto value : inputs) {
-            std::cout << value.left << ", " << value.right << std::endl;
+        std::int64_t sum = 0;
+
+        for (auto range: inputs) {
+            for (std::int64_t tag = range.left; tag <= range.right; ++tag) {
+                const std::string tagString = std::to_string(tag);
+                if (tagString.size() % 2 == 0) {
+                    std::string left = tagString.substr(0, tagString.size() / 2);
+                    std::string right = tagString.substr(tagString.size() / 2);
+                    if (left == right) {
+                        sum += tag;
+                    }
+                }
+            }
         }
 
-        return 0;
+        return sum;
+        // 82308124 too low
     }
 
-    std::int32_t Part2(const std::string_view input) {
-        return 0;
+    std::int64_t Part2(const std::string_view input) {
+        auto inputs = GetParsedInput(input);
+        std::int64_t sum = 0;
+
+        for (auto range: inputs) {
+            for (std::int64_t tag = range.left; tag <= range.right; ++tag) {
+                const std::string tagString = std::to_string(tag);
+                for (auto i = 1; i < tagString.size(); ++i) {
+                    if (i <= tagString.size() / 2) {
+                        const std::string substring = tagString.substr(0, i);
+                        bool invalid = true;
+                        for (int j = i; j < tagString.size(); j += i) {
+                            if (substring != tagString.substr(j, i)) {
+                                invalid = false;
+                                break;
+                            }
+                        }
+                        if (invalid) {
+                            sum += tag;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return sum;
     }
 }
 
